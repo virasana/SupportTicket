b50c3175f7f146abf87f14536c86cf59a8010fe8
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-11-15 17:49:48 +0000

    Working deployment to k8s with React and SupportTicket website with SQL Server database

--- Deployment/azure/ks-devops-up.bat
+++ Deployment/azure/ks-devops-up.bat
@@ -1,3 +1,10 @@
-echo "%~1"
-docker start "%~1"
-docker exec -it "%~1" /bin/bash 
+@REM echo "%~1"
+@REM Run this one first, if you don't have a running containerdocker
+		@REM docker run -it -v D:\DATA\GIT\SupportTicket\Deployment\azure\:/devops virasana/devops:latest
+		@REM -v enables volume mapping between windows and linux - work in notepad++ on Windows, and 
+		@REM see the changes effected in Linux.
+
+@REM if you already have a container (i.e. you have run the docker run -it command, above)		
+	@REM docker start "%~1"
+	@REM docker exec -it "%~1" /bin/bash 
+
--- streact/src/App.js
+++ streact/src/App.js
@@ -9,14 +9,16 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            &copy; Karunasoft Ltd 2018 
           </p>
+		  <p>1.0.0.14</p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
+		  
             Learn React
           </a>
         </header>
