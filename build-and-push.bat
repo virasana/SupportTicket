9970f1b211629def73af41df6bc49855aa2f2ee3
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-11-15 12:11:50 +0000

    Move some folders around

--- build-and-push.bat
+++
@@ -1,7 +0,0 @@
-SET THE_VERSION=virasana/stweb:1.0.0.%1
-
-echo Building %THE_VERSION% ...
-docker build . -f .\ST.Web\Dockerfile -t final 
-docker tag final %THE_VERSION%
-echo Pushing %THE_VERSION% ...
-docker push %THE_VERSION%
