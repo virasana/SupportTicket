5a1e3012ac9df596b81da8e014adef2ccd52737b
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-11-19 15:26:21 +0000

    vsts agent support

--- streact/build-and-push.bat
+++ streact/build-and-push.bat
@@ -1,6 +1,7 @@
-SET THE_VERSION=virasana/streact:1.0.0.%1
-
-echo Building %THE_VERSION% ...
-docker build . -f .\Dockerfile -t virasana/streact:1.0.0.%1
-echo Pushing %THE_VERSION% ...
-docker push %THE_VERSION%
+#!/bin/bash 
+THE_VERSION=virasana/streact:1.0.0.$1
+echo Building $THE_VERSION ...
+docker build . -f .\Dockerfile -t virasana/streact:1.0.0.$1
+echo Pushing $THE_VERSION ...
+docker push $THE_VERSION
+echo "done!"
---
+++ vsts/start-vs-agent.bat
@@ -0,0 +1,2 @@
+SET /P TOKEN= Please enter the VSTS Personal Access Token (PAT):
+docker run -e VSTS_ACCOUNT=karunasoft -e VSTS_TOKEN=%TOKEN% -e VSTS_POOL=SupportTicket --rm -d -it microsoft/vsts-agent:latest /vsts/start.sh
