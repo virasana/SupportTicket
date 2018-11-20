7732a65a14999c7fd9a7cfc3b5a56e36de0ea2c5
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-11-20 13:12:18 +0000

    Update Dockerfile path to Unix format

--- Web/streact/build-and-push.sh
+++ Web/streact/build-and-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 THE_VERSION=virasana/streact:1.0.0.$1
 echo Building $THE_VERSION ...
-docker build . -f .\Dockerfile -t virasana/streact:1.0.0.$1
+docker build . -f ./Dockerfile -t virasana/streact:1.0.0.$1
 echo Pushing $THE_VERSION ...
 docker push $THE_VERSION
 echo "done!"
