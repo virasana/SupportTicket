cabacca5837d1c6c744d218a2c1639d34622b085
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-11-21 11:33:54 +0000

    Support Ticket Release Tag

--- Deployment/azure/replace-environment-vars.sh
+++ Deployment/azure/replace-environment-vars.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Generating kubernetes template for this release"
-echo "Release Version ${SUPPORT_TICKET_VERSION}"
+echo "Release Tag ${SUPPORT_TICKET_RELEASE_TAG}"
+
 echo "File output: ./support-ticket-k8s.yaml"
 
 envsubst < ./_support-ticket-k8s.yaml > ./support-ticket-k8s.yaml
