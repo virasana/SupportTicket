d257fb78875761b6b6996e1aad064217fd71ee01
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-11-21 12:57:29 +0000

    Update tokens

--- Deployment/azure/replace-environment-vars.sh
+++ Deployment/azure/replace-environment-vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo '*********************************************************************'
 echo "Generating kubernetes template for this release"
-echo "Release Tag: ${SUPPORT_TICKET_RELEASE_TAG}"
+echo "Release Tag: ${SUPPORT_TICKET_REACT_IMAGE}"
 echo "Template Path:  ${1}"
 echo "Output Path: ${2}"
 echo '*********************************************************************'
