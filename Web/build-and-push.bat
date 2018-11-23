f2472022b938974edbb34eda4ca5f8f750f9c5fd
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-11-23 14:57:59 +0000

    Adding missing nuget packages - use dotnet add!

--- Web/build-and-push.bat
+++ Web/build-and-push.bat
@@ -1,7 +1,7 @@
-SET THE_VERSION=virasana/stweb:1.0.0.%1
+SET THE_VERSION=virasana/stweb:1.0.0.0 @REM %1
 
 echo Building %THE_VERSION% ...
 docker build . -f .\ST.Web\Dockerfile -t final 
 docker tag final %THE_VERSION%
-echo Pushing %THE_VERSION% ...
-docker push %THE_VERSION%
+@REM echo Pushing %THE_VERSION% ...
+@REM docker push %THE_VERSION%
