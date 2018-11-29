c4a814037deca7d84845345aa6d2341ad7a865e7
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-11-29 17:16:14 +0000

    Replace Tokens powershell script

---
+++ Deployment/Replace-Tokens.ps1
@@ -0,0 +1,21 @@
+﻿param(
+    [Parameter(Mandatory=$true)][string]$filePath,
+    [Parameter(Mandatory=$true)][string]$tokensStartingWith
+)
+
+Write-Host -Fore Yellow "Replacing Tokens";
+    
+$fileContents = [IO.File]::ReadAllText($filePath)
+
+    
+foreach($theEnv in (Get-ChildItem env:* | sort-object Name)){
+    if($theEnv.Name.StartsWith($tokensStartingWith))
+    {
+        Write-Host -Fore Yellow "Updating: $($theEnv.Name)=$($theEnv.Value)"
+        $fileContents = $fileContents.Replace("`${$($theEnv.Name)}", $theEnv.Value)
+    }
+}
+
+$fileContents | Out-File -Encoding utf8 -filePath $filePath | Out-Null
+Write-Host -Fore Yellow $fileContents
+
