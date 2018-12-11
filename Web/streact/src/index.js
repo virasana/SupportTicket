2590456fb26d39bd2fa77a284c3d395499a26b8d
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-12-11 18:16:53 +0000

    Tidy up imports to fix bugs

--- Web/streact/src/index.js
+++ Web/streact/src/index.js
@@ -1,16 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
-import promise from "redux-promise";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import reducers from "./reducers";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Root from './root';
-
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Root>
