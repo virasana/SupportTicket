45a0492ed67fba26382e67729068da28dd67ecbb
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-12-18 19:26:05 +0000

    Tokens

--- Deployment/Replace-Tokens-Test-Bed.ps1
+++ Deployment/Replace-Tokens-Test-Bed.ps1
@@ -2,11 +2,11 @@
 & $PSScriptRoot/Replace-Tokens.ps1 `
             -filePath (Join-Path -Path $PSScriptRoot -ChildPath "test.yaml") `
             -secrets `
-@"
+"
 {
-'SUPPORT_TICKET_DEPLOY_DB_CONN_STRING_AUTH':  'SomeDbAuthConnString',
-'SUPPORT_TICKET_DEPLOY_JWT_SECRET':  'SomeJwtSecret',
-'SUPPORT_TICKET_DEPLOY_DB_SA_PASSWORD':  'SomedbSaPassword',
-'SUPPORT_TICKET_DEPLOY_DB_CONN_STRING':  'SomeDbConnString'
+'SUPPORT_TICKET_DEPLOY_DB_CONN_STRING_AUTH':  '$secret1',
+'SUPPORT_TICKET_DEPLOY_JWT_SECRET':  '$secret2',
+'SUPPORT_TICKET_DEPLOY_DB_SA_PASSWORD':  '$secret3',
+'SUPPORT_TICKET_DEPLOY_DB_CONN_STRING':  '$secret4'
 }
-"@
+"
