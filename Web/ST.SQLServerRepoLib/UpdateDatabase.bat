109fc379bb6627001092cf3a1448136e591981e4
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-12-18 12:19:05 +0000

    Batch files upload

--- Web/ST.SQLServerRepoLib/UpdateDatabase.bat
+++ Web/ST.SQLServerRepoLib/UpdateDatabase.bat
@@ -3,6 +3,7 @@
 @REM note 127.0.0.1, below --> points to design-time sql server database, which you can access from the host machine
 @REM the db lives in the docker container
 
+cd %~dp0
 SET SUPPORT_TICKET_DEPLOY_DB_CONN_STRING=Data Source=127.0.0.1;Initial Catalog=SupportTicket;User Id=SA;password=D3vs123---D3vs123
 dotnet ef database update --context SupportTicketDbContext --project .\ST.SqlServerRepoLib.csproj --startup-project ..\ST.Web\ST.Web.csproj  --verbose
 
--- Web/ST.UsersRepoLib/AddMigration.bat
+++ Web/ST.UsersRepoLib/AddMigration.bat
@@ -1,3 +1,4 @@
+
 @REM Args: %1 --> Migration Name
 @REM Get the password from environment variable
 @REM We are migrating EF on Windows host, so need to set it up here too
@@ -6,6 +7,7 @@
 @REM the db lives in the docker container
 @REM see the IDesignTimeDbContextFactory instances in the ST.Web project
 
+cd %~dp0
 SET SUPPORT_TICKET_DEPLOY_DB_CONN_STRING_AUTH=Data Source=127.0.0.1;Initial Catalog=SupportTicketAuth;User Id=SA;password=D3vs123---D3vs123
 dotnet ef migrations add Initial --context UsersDbContext --project .\ST.UsersRepoLib.csproj --startup-project ..\ST.Web\ST.Web.csproj 
 
