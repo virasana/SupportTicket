29f77c3a8a6c6bba882ae09dfa156c337db41122
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-12-18 18:31:00 +0000

    Replace-Tokens to support receving an array.

---
+++ Deployment/Replace-Tokens-Test-Bed.ps1
@@ -0,0 +1,12 @@
+﻿cls
+& $PSScriptRoot/Replace-Tokens.ps1 `
+            -filePath (Join-Path -Path $PSScriptRoot -ChildPath "test.yaml") `
+            -secrets `
+@"
+{
+'SUPPORT_TICKET_DEPLOY_DB_CONN_STRING_AUTH':  'SomeDbAuthConnString',
+'SUPPORT_TICKET_DEPLOY_JWT_SECRET':  'SomeJwtSecret',
+'SUPPORT_TICKET_DEPLOY_DB_SA_PASSWORD':  'SomedbSaPassword',
+'SUPPORT_TICKET_DEPLOY_DB_CONN_STRING':  'SomeDbConnString'
+}
+"@
--- Deployment/Replace-Tokens.ps1
+++ Deployment/Replace-Tokens.ps1
@@ -1,21 +1,30 @@
 ﻿param(
     [Parameter(Mandatory=$true)][string]$filePath,
-    [Parameter(Mandatory=$true)][string]$tokensStartingWith
+    [Parameter(Mandatory=$true)][string]$secrets   
 )
 
 Write-Host -Fore Yellow "Replacing Tokens";
     
 $fileContents = [IO.File]::ReadAllText($filePath)
 
+$theSecrets = $secrets |ConvertFrom-JSON 
     
-foreach($theEnv in (Get-ChildItem env:* | sort-object Name)){
-    if($theEnv.Name.StartsWith($tokensStartingWith))
-    {
-        Write-Host -Fore Yellow "Updating: $($theEnv.Name)=$($theEnv.Value)"
-        $fileContents = $fileContents.Replace("`${$($theEnv.Name)}", $theEnv.Value)
-    }
+#foreach($theEnv in (Get-ChildItem env:* | sort-object Name)){
+#    if($theEnv.Name.StartsWith($tokensStartingWith))
+#    {
+#        Write-Host -Fore Yellow "Updating: $($theEnv.Name)=$($theEnv.Value)"
+#        $fileContents = $fileContents.Replace("`${$($theEnv.Name)}", $theEnv.Value)
+#    }
+#}
+
+
+foreach($theSecret in $theSecrets.PSObject.Properties){
+    Write-Host -Fore Yellow "Updating: $($theSecret.Name)=$($theSecret.Value)"
+    $fileContents = $fileContents.Replace("`${$($theSecret.Name)}", $theSecret.Value)
 }
 
-$fileContents | Out-File -Encoding utf8 -filePath $filePath | Out-Null
-Write-Host -Fore Yellow $fileContents
+$fileContents
+
+#$fileContents | Out-File -Encoding utf8 -filePath $filePath | Out-Null
+#Write-Host -Fore Yellow $fileContents
 
