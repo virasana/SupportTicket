79ab7192b1849589090cffc764e96abbca194210
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2018-12-18 18:33:26 +0000

    Arrays

--- Deployment/Replace-Tokens.ps1
+++ Deployment/Replace-Tokens.ps1
@@ -7,17 +7,19 @@ Write-Host -Fore Yellow "Replacing Tokens";
     
 $fileContents = [IO.File]::ReadAllText($filePath)
 
-$theSecrets = $secrets |ConvertFrom-JSON 
-    
-#foreach($theEnv in (Get-ChildItem env:* | sort-object Name)){
-#    if($theEnv.Name.StartsWith($tokensStartingWith))
-#    {
-#        Write-Host -Fore Yellow "Updating: $($theEnv.Name)=$($theEnv.Value)"
-#        $fileContents = $fileContents.Replace("`${$($theEnv.Name)}", $theEnv.Value)
-#    }
-#}
+# Iterate Environment Variables
+foreach($theEnv in (Get-ChildItem env:* | sort-object Name)){
+    if($theEnv.Name.StartsWith($tokensStartingWith))
+    {
+        Write-Host -Fore Yellow "Updating: $($theEnv.Name)=$($theEnv.Value)"
+        $fileContents = $fileContents.Replace("`${$($theEnv.Name)}", $theEnv.Value)
+    }
+}
 
 
+# Iterate secrets - VSTS won't pass these 
+#      into the environment - we need to use $secrets
+$theSecrets = $secrets | ConvertFrom-JSON 
 foreach($theSecret in $theSecrets.PSObject.Properties){
     Write-Host -Fore Yellow "Updating: $($theSecret.Name)=$($theSecret.Value)"
     $fileContents = $fileContents.Replace("`${$($theSecret.Name)}", $theSecret.Value)
